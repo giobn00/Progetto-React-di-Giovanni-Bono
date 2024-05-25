4d8860004a2d1ad51ba5518caf252876f6ebb00b
Giovanni <me@giovannibono.com>
2024-05-25 16:18:08 +0200

    nascosta API Key

--- src/routes/RecipeDetail.jsx
+++ src/routes/RecipeDetail.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from 'react-router';
 import useSWR from "swr";
 
-const API_Key = "c9602d561aa44e76949b04c6f0ba2403";
+const API_Key = import.meta.env.VITE_API_KEY ;
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const RecipeDetail = () => {
--- src/routes/RecipeList.jsx
+++ src/routes/RecipeList.jsx
@@ -8,7 +8,8 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-const API_Key = "c9602d561aa44e76949b04c6f0ba2403";
+
+const API_Key = import.meta.env.VITE_API_KEY ;
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const RecipeList = () => {
